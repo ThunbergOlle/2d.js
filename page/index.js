2b6dddf44610b61643eae637d88019dbc73765d9
ThunbergOlle <olle.thunberg03@gmail.com>
2018-10-06 13:53:19 +0200

    Working :green_heart:

--- page/index.js
+++ page/index.js
@@ -1,7 +1,20 @@
-const game = document.getElementById('game');
+const gamediv = document.getElementById('game');
 
+class Game {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+    }
 
-class hpBar {
+    init(backgroundImage, backgroundColor) {
+        gamediv.style.width = this.width + 'px';
+        gamediv.style.height = this.height + 'px';
+        gamediv.style.zIndex -= 5;
+        gamediv.style.position = 'relative'
+        gamediv.style.backgroundColor = backgroundColor;
+    }
+}
+class HpBar {
     constructor() {
         this.maxhp = 100;
         this.minhp = 0;
@@ -16,34 +29,40 @@ class hpBar {
     }
 }
 class Sprite {
-    constructor(x, y, h, w, src, staticIn) {
+    constructor(x, y, h, w, src) {
         this.x = x;
         this.y = y;
         this.width = w;
         this.height = h;
         this.src = src;
 
-        this.img;
-        this.static = staticIn;
+        this.img = 0;
 
+        this.player = false;
+        this.steps = 10;
+        this.static = false;
+        this.input = 'wasd';
     }
 
     render() {
         this.img = document.createElement("img");
         this.img.src = this.src;
         this.img.style.position = 'absolute';
+        this.img.style.left = this.x + 'px';
+        this.img.style.top = this.y + 'px';
         this.img.style.zIndex = -1;
         console.log(this.height);
         this.img.style.height = this.height + 'px';
         this.img.style.width = this.width + 'px';
 
         this.img.onload = () => {
-            game.appendChild(this.img);
+            gamediv.appendChild(this.img);
         }
 
 
     }
-    move(x, y) {
+
+    moveForce(x, y) {
         console.log("MOVE");
         if (!this.static) {
             console.log(this.static);
@@ -53,5 +72,43 @@ class Sprite {
             this.img.style.left = this.x + 'px';
             console.log(this.img);
         }
+    }
+    move() {
+        if (!this.static && this.input === 'wasd' && this.player === true) {
+            if (document.onkeydown = 37) {
+                console.log("123");
+            }
+            let input = (key) => {
+                console.log(key);
+                let parseWidth = gamediv.style.width.toString();
+                parseWidth = parseWidth.replace("px", "");
+                let parseHeight = gamediv.style.height.toString();
+                parseHeight = parseHeight.replace("px", "");
+                if (key === 'ArrowRight' && this.x < parseWidth - this.width) {
+                    this.x += 10;
+                    this.img.style.left = this.x + 'px';
+                } else if (key === 'ArrowLeft' && this.x > 0) {
+                    this.x -= 10;
+                    this.img.style.left = this.x + 'px';
+                } else if (key === 'ArrowDown' && this.y < parseHeight - this.height) {
+                    this.y += 10;
+                    this.img.style.top = this.y + 'px';
+                } else if (key === 'ArrowUp' && this.y > 0) {
+                    this.y -= 10;
+                    this.img.style.top = this.y + 'px';
+                }
+
+
+            }
+
+            window.addEventListener('keydown', function (e) {
+                // ERROR IS CAUSED BECAUSE "THIS" CAN NOT BE USED INSIDE AN EVENT LISTERNER: THEN ITS REFFERED TO AS THIS.event or whatever.
+                const getkey = e.key; // "a", "1", "Shift", etc
+                input(getkey);
+            });
+
+
+        }
+
     }
 }
